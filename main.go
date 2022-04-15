0941a578006894a2cce1ef2ba389f442e60d6444
bneuenfeldt <neuenfeldt.benedikt@gmail.com>
2022-04-15 09:47:39 +0900

    fixed command line arguments

--- main.go
+++ main.go
@@ -9,23 +9,26 @@ import (
 var (
 	sourceDirectory string
 	outDirectory    string
-	languages       string
+	generator       string
 )
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use: "browserd",
+		Use: "protogen",
 		Run: func(cmd *cobra.Command, args []string) {
-			generator := CreateGenerator(sourceDirectory, outDirectory, languages)
+			generator := CreateGenerator(sourceDirectory, outDirectory, generator)
 			if err := generator.Run(); err != nil {
 				fmt.Println("failed to generate code: ", err)
 			}
 		},
 	}
 
-	rootCmd.PersistentFlags().StringVar(&sourceDirectory, "source", ".", "source directory / files")
+	rootCmd.PersistentFlags().StringVar(&sourceDirectory, "source", "", "source directory / files")
 	rootCmd.PersistentFlags().StringVar(&outDirectory, "out", "", "output directory")
-	rootCmd.PersistentFlags().StringVar(&languages, "languages", "", "comma-separated list of languages that should be generated")
+	rootCmd.PersistentFlags().StringVar(&generator, "generator", "", "directory containing the generator")
+	_ = rootCmd.MarkPersistentFlagRequired("source")
+	_ = rootCmd.MarkPersistentFlagRequired("out")
+	_ = rootCmd.MarkPersistentFlagRequired("generator")
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
